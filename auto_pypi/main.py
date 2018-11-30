ae17e7a54f4dd9df717cfeb9decf04225cb817da
LEI Sen <Listen180@gmail.com>
2018-11-30 13:30:34 +0800

    upload empty file

---
+++ auto_pypi/main.py
@@ -0,0 +1,13 @@
+#!/Users/leisen/anaconda3/bin/python3
+# -*- coding: UTF-8 -*-
+
+# ********************************************************
+# * Author        : LEI Sen
+# * Email         : sen.lei@outlook.com
+# * Create time   : 2018-11-30 13:30
+# * Last modified : 2018-11-30 13:30
+# * Filename      : main.py
+# * Description   : 
+# *********************************************************
+
+
