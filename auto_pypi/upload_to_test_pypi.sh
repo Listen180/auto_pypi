e351cf6ddadbb1fed40787058b1cc1ab6f33427f
LEI Sen <Listen180@gmail.com>
2018-12-03 16:27:23 +0800

    new upload

---
+++ auto_pypi/upload_to_test_pypi.sh
@@ -0,0 +1,12 @@
+# 
+# *********************************************************
+# * Author        : LEI Sen
+# * Email         : sen.lei@outlook.com
+# * Create time   : 2018-12-03 16:24
+# * Last modified : 2018-12-03 16:24
+# * Filename      : upload_to_test_pypi.sh
+# * Description   : 
+# *********************************************************
+
+
+twine upload --repository-url https://test.pypi.org/legacy/ dist/*
