f8b4c1ccd64463d865b3ec6f4fe5d77c05e2e1eb
listen <listen180@gmail.com>
2018-12-27 12:05:19 +0800

    change dependency

--- setup.py
+++ setup.py
@@ -38,9 +38,6 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=[
         'click',
-        'subprocess',
-        'shlex',
-        'platform',
     ],
     license="BSD 3-clause",
     classifiers=[
